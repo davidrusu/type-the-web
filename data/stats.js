6a972416985e848e2c19043fd76e839a4afa0bed
David Rusu <davidrusu.me@gmail.com>
2014-12-22 15:33:59 -0500

    switched to sidebar from panel for stats

--- data/stats.js
+++ data/stats.js
@@ -1,20 +1,11 @@
-$("#ttw-stop-button").click((e) => self.port.emit('stop'));
-
-$("#ttw-skip-button").click((e) => {
-    self.port.emit('skip');
-    // We need to blur the button, because it will remain focused
-    // after being clicked and pressing space will push the button. 
-    $('#ttw-skip-button').blur(); 
-});
-
-self.port.on("stop", () => {
+addon.port.on("stop", () => {
     // we disable the buttons to avoid any confusion because the
     // stats panel will remain active but the typing test is done
     $("#ttw-stop-button").attr("disabled", "disabled");
     $("#ttw-skip-button").attr("disabled", "disabled");
 });
 
-self.port.on("ttw-stats-updated", (hudStats) => {
+addon.port.on("stats-updated", (hudStats) => {
     var timeSec = hudStats.timeSoFar / 1000; // time is in millis
     var burstwpm = Math.floor(
         hudStats.keysBurst.length / CONSTANTS.WORD_LENGTH / CONSTANTS.BURST_TIME * 60);
