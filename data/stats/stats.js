7b735f1eca83e77bb9aa8da4f319737fd6387ecc
David Rusu <davidrusu.me@gmail.com>
2015-01-27 00:18:27 -0500

    switched to smoothie charts

--- data/stats/stats.js
+++ data/stats/stats.js
@@ -1,147 +1,24 @@
-var data = {
-    labels: [],
-    datasets: [
-        {
-            label: "Burst Errors",
-            strokeColor: "rgba(230, 30, 30, 1)",
-            data: []
-        },
-        {
-            label: "Burst WPM",
-            strokeColor: "rgba(230, 120, 30, 1)",
-            data: []
-        }
-    ]
-};
 $(document).on('ready', () => {
-    Chart.defaults.global = {
-        // Boolean - Whether to animate the chart
-        animation: false,
+    var chart = new SmoothieChart({ millisPerPixel:273
+                                  , interpolation:'bezier'
+                                  , grid: { fillStyle:'#ffffff'
+                                          , strokeStyle:'rgba(0,0,0,0.10)'
+                                          , verticalSections:3
+                                          , borderVisible:false }
+                                  , labels: { fillStyle:'#777777'
+                                            , fontSize:15
+                                            , precision:0 }
+                                  //, timestampFormatter: SmoothieChart.timeFormatter
+                                  , minValue:0});
 
-        // Number - Number of animation steps
-        animationSteps: 0,
+    var canvas = document.getElementById("wpm-chart");
+    var wpmSeries = new TimeSeries();
+    var errorSeries = new TimeSeries();
 
-        // String - Animation easing effect
-        animationEasing: "easeOutQuart",
+    chart.addTimeSeries(wpmSeries, {lineWidth:2,strokeStyle:'#97e819'});
+    chart.addTimeSeries(errorSeries, {lineWidth:2,strokeStyle:'#ffa819'});
 
-        // Boolean - If we should show the scale at all
-        showScale: true,
-
-        // Boolean - If we want to override with a hard coded scale
-        scaleOverride: false,
-
-        // ** Required if scaleOverride is true **
-        // Number - The number of steps in a hard coded scale
-        scaleSteps: null,
-        // Number - The value jump in the hard coded scale
-        scaleStepWidth: null,
-        // Number - The scale starting value
-        scaleStartValue: null,
-
-        // String - Colour of the scale line
-        scaleLineColor: "rgba(0,0,0,.1)",
-
-        // Number - Pixel width of the scale line
-        scaleLineWidth: 1,
-
-        // Boolean - Whether to show labels on the scale
-        scaleShowLabels: true,
-
-        // Interpolated JS string - can access value
-        scaleLabel: "<%=value%>",
-
-        // Boolean - Whether the scale should stick to integers, not floats even if drawing space is there
-        scaleIntegersOnly: true,
-
-        // Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-        scaleBeginAtZero: false,
-
-        // String - Scale label font declaration for the scale label
-        scaleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-
-        // Number - Scale label font size in pixels
-        scaleFontSize: 12,
-
-        // String - Scale label font weight style
-        scaleFontStyle: "normal",
-
-        // String - Scale label font colour
-        scaleFontColor: "#666",
-
-        // Boolean - whether or not the chart should be responsive and resize when the browser does.
-        responsive: true,
-
-        // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-        maintainAspectRatio: true,
-
-        // Boolean - Determines whether to draw tooltips on the canvas or not
-        showTooltips: false,
-
-        // Array - Array of string names to attach tooltip events
-        tooltipEvents: [],
-
-        // String - Tooltip background colour
-        tooltipFillColor: "rgba(0,0,0,0.8)",
-
-        // String - Tooltip label font declaration for the scale label
-        tooltipFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-
-        // Number - Tooltip label font size in pixels
-        tooltipFontSize: 14,
-
-        // String - Tooltip font weight style
-        tooltipFontStyle: "normal",
-
-        // String - Tooltip label font colour
-        tooltipFontColor: "#fff",
-
-        // String - Tooltip title font declaration for the scale label
-        tooltipTitleFontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-
-        // Number - Tooltip title font size in pixels
-        tooltipTitleFontSize: 14,
-
-        // String - Tooltip title font weight style
-        tooltipTitleFontStyle: "bold",
-
-        // String - Tooltip title font colour
-        tooltipTitleFontColor: "#fff",
-
-        // Number - pixel width of padding around tooltip text
-        tooltipYPadding: 6,
-
-        // Number - pixel width of padding around tooltip text
-        tooltipXPadding: 6,
-
-        // Number - Size of the caret on the tooltip
-        tooltipCaretSize: 8,
-
-        // Number - Pixel radius of the tooltip border
-        tooltipCornerRadius: 6,
-
-        // Number - Pixel offset from point x to tooltip edge
-        tooltipXOffset: 10,
-
-        // String - Template string for single tooltips
-        tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
-
-        // String - Template string for single tooltips
-        multiTooltipTemplate: "<%= value %>",
-
-        // Function - Will fire on animation progression.
-        onAnimationProgress: function(){},
-
-        // Function - Will fire on animation completion.
-        onAnimationComplete: function(){}
-    };
-                               
-    
-    var ctx = document.getElementById("wpm-chart").getContext("2d");
-    var wpmChart = new Chart(ctx).Line(data, {
-        bezierCurve: false,
-        pointDot: false,
-        datasetFill: false
-    });
+    chart.streamTo(canvas, 0);
 
     addon.port.on("stop", () => {
         // we disable the buttons to avoid any confusion because the
@@ -163,15 +40,19 @@ $(document).on('ready', () => {
         $('#errors').text(hudStats.errorsTyped);
         $('#burst').text(burstwpm);
         $('#wpm').text(wpm);
-        if (timeSec - lastUpdateTime > 1) {
-            lastUpdateTime = timeSec;
-	    numTicks += 1;
-	    if (numTicks > 30) {
-		wpmChart.removeData();
-		numTicks -= 1;
-	    }
-            wpmChart.addData([bursterrors, burstwpm], '');
-            wpmChart.update();
-        }
+
+        wpmSeries.append(new Date().getTime(), burstwpm);
+        errorSeries.append(new Date().getTime(), bursterrors);
+
+        //if (timeSec - lastUpdateTime > 1) {
+            //lastUpdateTime = timeSec;
+	    //numTicks += 1;
+	    //if (numTicks > 30) {
+		//wpmChart.removeData();
+		//numTicks -= 1;
+	    //}
+            //wpmChart.addData([bursterrors, burstwpm], '');
+            //wpmChart.update();
+        //}
     });
 });
